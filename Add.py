f919d3509bf9a65942531e785a128d7b09707949
NiharikaAnnepaka <annepakaniharika@gmail.com>
2025-03-03 15:25:30 +0530

    commit

--- Add.py
+++ Add.py
@@ -3,10 +3,9 @@ a=200
 b=300
 sum = a+b
 diff = a-b
-mult = a*b
+
 div = a/b
 print("sum of two numbers is:",sum)
 print("diff of two numbers is:",sum)
-print("mult of two numbers is :",mult)
 
 print("div of two numbers is :",div)
